28c2fd8d7b6790cd99df659293823f30f9b0761b
amarchan-stratio <amarchan@stratio.com>
2023-01-12 16:03:52 +0100

    First prototype

---
+++ app/app.py
@@ -0,0 +1,516 @@
+import logging
+import sys
+import os
+import re
+import base64
+import zipfile
+import tempfile
+import calendar
+from datetime import datetime
+import pandas as pd
+import plotly.express as px
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+from spacy.lang.es import Spanish
+nlp = Spanish()
+
+STOPWORDS = []
+
+with open('app/assets/stopwords-es.txt', 'r') as f:
+    STOPWORDS = f.read().splitlines()
+
+STOPWORDS += ['(', ')', '.', '"', "'", ',', ':', ';', '?', '¿', '¡', '!', 'º', 'ª', '%', '/', "\\", '*', '+', '-',
+        '=', '#', '€', '-', '_', '\n', '&', '@', '[', ']', '>', '<', "'", '\t', '️']
+
+# Init logging
+logging.basicConfig(
+    format='[%(asctime)s] [%(name)s:%(lineno)s] [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S %z',
+    stream=sys.stdout,
+    level=10
+)
+
+log = logging.getLogger("app")
+log.setLevel(logging.INFO)
+
+from dash_extensions.enrich import DashProxy, MultiplexerTransform, LogTransform, NoOutputTransform
+from dash_extensions.enrich import Input, Output, State, html, dcc, dash_table
+import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
+
+app = DashProxy(
+    __name__, 
+    title="WhatsApp Wrapped", 
+    transforms=[
+        MultiplexerTransform(),  # makes it possible to target an output multiple times in callbacks
+        NoOutputTransform(),
+        LogTransform()  # makes it possible to write log messages to a Dash component
+    ],
+    external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME],
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+    ],
+)
+app.config.suppress_callback_exceptions = True
+server = app.server
+
+app.layout = html.Div(
+    [
+        dcc.Store(id="words-store"),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.H1(["Welcome to WhatsApp Wrapped"]),
+                    width="auto"
+                ),
+            ],
+            justify="center",
+            align="center",
+            style={
+                'margin-top': '15px',
+                'text-align': 'center'
+            }
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dcc.Upload(
+                            id='upload-chat',
+                            children=html.Div(
+                                [
+                                    'Drop or ',
+                                    html.A('Select the exported chat (ZIP)', style={"cursor": "pointer", 'color': 'var(--bs-primary)'})
+                                ]
+                            ),
+                            accept="zip,application/octet-stream,application/zip,application/x-zip,application/x-zip-compressed",
+                        )
+                    ],
+                    width="12"
+                ),
+            ],
+            justify="center",
+            align="center",
+            style={
+                'margin-bottom': '20px',
+                'margin-top': '15px'
+            }
+        ),
+        html.Div(
+                [
+                dbc.Row(
+                    [
+                        html.H4(
+                            id='n-messages',
+                            style={
+                                'margin-top': '35px',
+                                'text-align': 'center'
+                            }
+                        ),
+                        dcc.Loading(
+                            dcc.Graph(
+                                id='n-messages-by-person-graph',
+                            ),
+                        ),
+                    ]
+                ),
+                dbc.Row(
+                    [
+                        dcc.Loading(
+                            dcc.Graph(
+                                id='n-messages-by-month-and-person-graph',
+                            ),
+                        )
+                    ]
+                ),
+                dbc.Row(
+                    [
+                        dcc.Loading(
+                            dcc.Graph(
+                                id='n-messages-by-year-and-person-graph',
+                            ),
+                        )
+                    ]
+                ),
+                dbc.Row(
+                    [
+                        dcc.Loading(
+                            dcc.Graph(
+                                id='n-messages-by-hour-and-person-graph',
+                            ),
+                        )
+                    ]
+                ),
+                dbc.Row(
+                    [
+                        html.H4(
+                            id='n-words',
+                            style={
+                                'margin-top': '35px',
+                                'text-align': 'center'
+                            }
+                        ),
+                        dcc.Loading(
+                            dcc.Graph(
+                                id='most-repeated-words-graph',
+                            ),
+                        )
+                    ]
+                ),
+                dbc.Row(
+                    [
+                        dmc.Select(
+                            label="Choose a person",
+                            id="person-select",
+                            searchable=True,
+                            class_name="select-inpt",
+                            zIndex=9999
+                        ),
+                        dcc.Loading(
+                            dcc.Graph(
+                                id='most-repeated-words-by-person-graph',
+                            ),
+                        )
+                    ],
+                    justify="center",
+                    align="center"
+                ),
+                dbc.Row(
+                    [
+                        dcc.Loading(
+                            dcc.Graph(
+                                id='files-by-type-graph',
+                            ),
+                        )
+                    ]
+                ),
+            ],
+            id="graphs-div",
+            style={
+                'display': 'none'
+            }
+        )
+    ],
+    style={
+        'margin-bottom': '150px'
+    }
+)
+
+def create_bar_plot(df, x, y, color, title, x_title, y_title, legend_title, showlegend=True):
+    fig = px.bar(
+        df, 
+        x=x, 
+        y=y,
+        title=title,
+        color=color, 
+        # color_discrete_map=color_dict
+    )
+    fig.update_layout(
+        xaxis_title=x_title, 
+        yaxis_title=y_title,
+        legend_title=legend_title,
+        showlegend=showlegend
+    )
+
+    return fig
+
+
+def get_words_df(chat_df):
+    messages_df = chat_df[
+        (chat_df['message'] != 'audio omitido') &
+        (chat_df['message'] != 'sticker omitido') &
+        (chat_df['message'] != 'Video omitido') &
+        (chat_df['message'] != 'imagen omitida') &
+        (chat_df['message'] != 'GIF omitido')
+    ][['person', 'message']]
+
+    wordle_words = [
+        'https://wordle.danielfrg.com',
+        'https://lapalabradeldia.com/',
+        'Wordle',
+        'HeardlEsp'
+    ]
+
+    words = []
+
+    for row in messages_df.values:
+        person = row[0]
+        message = row[1]
+
+        # Remove Wordle messages
+        if any([word in message for word in wordle_words]):
+            continue
+
+        for token in nlp(message):
+            word = token.text.lower().strip()
+
+            if  word != '' and word not in STOPWORDS:
+                words.append({
+                    'person': person,
+                    'word': word
+                })
+
+    words_df = pd.DataFrame(words)
+
+    return words_df
+
+@app.callback(
+    Output('graphs-div', 'style'),
+    Output('n-messages', 'children'),
+    Output('n-messages-by-person-graph', 'figure'),
+    Output('n-messages-by-month-and-person-graph', 'figure'),
+    Output('n-messages-by-year-and-person-graph', 'figure'),
+    Output('n-messages-by-hour-and-person-graph', 'figure'),
+    Output('n-words', 'children'),
+    Output('words-store', 'data'),
+    Output('person-select', 'data'),
+    Output('person-select', 'value'),
+    Output('files-by-type-graph', 'figure'),
+    Input('upload-chat', 'contents'),
+    State('upload-chat', 'filename'),
+    prevent_initial_call=True
+)
+def upload_chat(file, filename):
+    fd, path = tempfile.mkstemp(suffix='.zip')
+
+    chat = ''
+
+    try:
+        file = file.split('base64,')[1]
+
+        with os.fdopen(fd, 'wb') as tmp:
+            tmp.write(base64.b64decode(file))
+
+        archive = zipfile.ZipFile(path, 'r')
+        chat = archive.read('_chat.txt').decode()
+        archive.close()
+    except Exception as e:
+        print('The following error happened wile reading the ZIP file: ', e)
+    finally:
+        os.remove(path)
+
+    messages = []
+    try:
+        for line in chat.split('\n'):
+            # Remove LTR mark
+            line = line.replace('‎', '').replace('\r', '')
+            # Extract the date and time, sender name and message sent
+            regex_res = re.findall(r'\[(.+?)\] (.+?): (.*)', line)
+            if len(regex_res) > 0:
+                date, person, message = regex_res[0]
+                # Convert the date and time into a datetime object
+                date = datetime.strptime(date, '%d/%m/%y, %H:%M:%S')
+                if person != 'Tú' and 'Los mensajes y las llamadas están cifrados de extremo a extremo. Nadie fuera de este chat, ni siquiera WhatsApp, puede leerlos ni escucharlos.' not in message:
+                    messages.append({
+                        'date': date,
+                        'person': person,
+                        'message': message
+                    })
+            elif len(messages) > 0:
+                messages[-1]['message'] += ' ' + line
+    except Exception as e:
+        print('The following error happened wile parsing the chat: ', e)
+
+    chat_df = pd.DataFrame(messages)
+
+    n_messages_by_person_df = chat_df \
+                                .groupby('person') \
+                                .agg(n_messages = ('person', 'size')) \
+                                .reset_index() \
+                                .sort_values('n_messages', ascending = False)
+
+    n_messages_by_person_fig = create_bar_plot(
+        n_messages_by_person_df, 
+        x='person', 
+        y='n_messages',
+        color='person',
+        title='Número de mensajes enviados por persona',
+        x_title="Persona",
+        y_title="Número de mensajes",
+        legend_title="Persona",
+        showlegend=False
+    )
+
+    n_messages_by_month_and_person_df = chat_df \
+                                            .groupby([chat_df.person, chat_df.date.dt.month]) \
+                                            .agg(n_messages = ('person', 'size')) \
+                                            .reset_index() \
+                                            .sort_values(['date', 'person'])
+    n_messages_by_month_and_person_df['month_name'] = n_messages_by_month_and_person_df['date'] \
+                                                        .apply(lambda x: calendar.month_name[x].capitalize())
+
+    n_messages_by_month_and_person_fig = create_bar_plot(
+        n_messages_by_month_and_person_df, 
+        x='month_name', 
+        y='n_messages',
+        color='person',
+        title='Número de mensajes enviados por mes por persona',
+        x_title="Persona",
+        y_title="Número de mensajes",
+        legend_title="Persona",
+    )
+
+    n_messages_by_year_and_person_df = chat_df \
+                                            .groupby([chat_df.person, chat_df.date.dt.year]) \
+                                            .agg(n_messages = ('person', 'size')) \
+                                            .reset_index() \
+                                            .sort_values(['date', 'person'])
+
+    n_messages_by_year_and_person_fig = create_bar_plot(
+        n_messages_by_year_and_person_df, 
+        x='date', 
+        y='n_messages',
+        color='person',
+        title='Número de mensajes enviados por año por persona',
+        x_title="Persona",
+        y_title="Número de mensajes",
+        legend_title="Persona",
+    )
+
+    n_messages_by_hour_and_person_df = chat_df \
+                                            .groupby([chat_df.person, chat_df.date.dt.hour]) \
+                                            .agg(n_messages = ('person', 'size')) \
+                                            .reset_index() \
+                                            .sort_values(['date', 'person'])
+
+    n_messages_by_hour_and_person_fig = create_bar_plot(
+        n_messages_by_hour_and_person_df, 
+        x='date', 
+        y='n_messages',
+        color='person',
+        title='Número de mensajes enviados por hora por persona',
+        x_title="Persona",
+        y_title="Número de mensajes",
+        legend_title="Persona",
+    )
+
+    aux_df = chat_df \
+                .groupby([chat_df.person, chat_df.date.dt.date]) \
+                .agg(n_messages = ('person', 'size')) \
+                .reset_index() \
+                .sort_values(['date', 'person'])
+
+    # avg_messages_in_day_by_person_df = aux_df \
+    #                                     .groupby('person') \
+    #                                     .agg(
+    #                                         avg_messages = ('n_messages', 'mean'), 
+    #                                         max_messages = ('n_messages', 'max'),
+    #                                         min_messages = ('n_messages', 'min'),
+    #                                     ) \
+    #                                     .reset_index() \
+    #                                     .sort_values('avg_messages', ascending = False)
+
+    files_df = chat_df[
+        (chat_df['message'] == 'audio omitido') |
+        (chat_df['message'] == 'sticker omitido') |
+        (chat_df['message'] == 'Video omitido') |
+        (chat_df['message'] == 'imagen omitida') |
+        (chat_df['message'] == 'GIF omitido')
+    ][['person', 'message']]
+
+    files_df['file'] = files_df['message'].apply(lambda x: x.replace(' omitido', '').replace(' omitida', ''))
+
+    files_by_type_df = files_df \
+                .groupby(['person', 'file']) \
+                .agg(n_files = ('person', 'size')) \
+                .reset_index()
+
+    files_by_type_fig = create_bar_plot(
+        files_by_type_df, 
+        x='file', 
+        y='n_files',
+        color='person',
+        title='Archivos enviados en la conversación por persona',
+        x_title="Tipo de archivo",
+        y_title="Número de veces enviado",
+        legend_title="Persona",
+    )
+
+    person_data = []
+    for person in chat_df['person'].unique():
+        person_data.append(
+            {
+                'label': person,
+                'value': person,
+            }
+        )
+
+    person_data.sort(key=lambda x: x['label'])
+
+    words_df = get_words_df(chat_df)
+
+    return  None, \
+            f'La conversación tiene {chat_df.shape[0]} mensajes enviados.', \
+            n_messages_by_person_fig, \
+            n_messages_by_month_and_person_fig, \
+            n_messages_by_year_and_person_fig, \
+            n_messages_by_hour_and_person_fig, \
+            f'La conversación tiene {words_df.shape[0]} palabras enviadas quitando stopwords y símbolos.', \
+            words_df.to_dict('records'), \
+            person_data, \
+            person_data[0]['value'], \
+            files_by_type_fig
+
+@app.callback(
+    Output('most-repeated-words-graph', 'figure'),
+    Input('words-store', 'data'),
+    prevent_initial_call=True
+)
+def load_words_graphs(words_records):
+    words_df = pd.DataFrame(words_records)
+
+    most_repeated_words_df = words_df \
+            .groupby(['word']) \
+            .agg(n_times = ('word', 'size')) \
+            .reset_index() \
+            .sort_values('n_times', ascending=False) \
+            .head(30)
+
+    most_repeated_words_fig = px.bar(
+        most_repeated_words_df, 
+        x='word', 
+        y='n_times',
+        title='Palabras más repetidas en la conversación',
+    )
+    most_repeated_words_fig.update_layout(
+        xaxis_title="Palabra", 
+        yaxis_title="Número de veces repetida",
+    )
+
+    most_repeated_words_fig.update_xaxes(tickangle=30)
+    
+    return most_repeated_words_fig
+
+@app.callback(
+    Output('most-repeated-words-by-person-graph', 'figure'),
+    Input('person-select', 'value'),
+    State('words-store', 'data'),
+    prevent_initial_call=True
+)
+def load_interactive_words_graphs(person, words_records):
+    words_df = pd.DataFrame(words_records)
+
+    most_repeated_words_by_person_df = words_df \
+            .groupby(['person', 'word']) \
+            .agg(n_times = ('person', 'size')) \
+            .reset_index() \
+            .sort_values('n_times', ascending=False)
+
+    most_repeated_words_by_person_fig = px.bar(
+        most_repeated_words_by_person_df[most_repeated_words_by_person_df['person'] == person].sort_values(by=['n_times'], ascending=False).head(30), 
+        x='word', 
+        y='n_times',
+        title=f'Palabras más repetidas en la conversación por {person}',
+    )
+    most_repeated_words_by_person_fig.update_layout(
+        xaxis_title="Palabra", 
+        yaxis_title="Número de veces repetida",
+    )
+    most_repeated_words_by_person_fig.update_xaxes(tickangle=30)
+    
+    return most_repeated_words_by_person_fig
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, host="0.0.0.0", port=8080, use_reloader=False)
